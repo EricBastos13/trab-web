8fb7052bcc12e1b949b69719353927430ea5d560
EricBastos13 <03ericsilva0@gmail.com>
2024-10-16 23:19:49 -0300

    Add files via upload

--- style site/script/script.js
+++ style site/script/script.js
@@ -1,1 +1,24 @@
+//barra lateral de menu
+document.getElementById("atvmenu").addEventListener("click", function() {
+    const sidebar = document.getElementById("barraltr");
+    sidebar.classList.toggle("atv");
+});
 
+//transicao na pagina
+const links = document.querySelectorAll('.nav-link');
+const pages = document.querySelectorAll('.page');
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede o comportamento padrão do link
+
+        // Esconde todas as páginas
+        pages.forEach(page => {
+            page.classList.remove('active');
+        });
+
+        // Mostra a página correspondente ao link clicado
+        const targetPage = document.getElementById(this.dataset.target);
+        targetPage.classList.add('active');
+    });
+});
