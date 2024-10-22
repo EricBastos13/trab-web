be0f4a736f8d6599efdbfcbe11f369fd4f3fd685
EricBastos13 <03ericsilva0@gmail.com>
2024-10-21 23:14:43 -0300

    Add files via upload

--- style site/script/script.js
+++ style site/script/script.js
@@ -22,4 +22,12 @@ links.forEach(link => {
         targetPage.classList.add('active');
     });
 });
+// Selecionar o botão e o conteúdo
+const accordionButton = document.querySelector('.topicos');
+const accordionContent = document.querySelector('.content');
 
+// Adicionar evento de clique ao botão
+accordionButton.addEventListener('click', function () {
+    // Alterna a exibição do conteúdo
+    accordionContent.classList.toggle('show');
+});
